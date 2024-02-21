45ce403cec23f2e65c9b7027ee3b29ea0627cddb
katerynazhvanko <kateryna.tertychna@gmail.com>
2024-02-21 09:58:10 +0200

    ain

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
 import { useState, useEffect } from "react";
 
-import Descriptions from "./components/Description/Description";
+import Description from "./components/Description/Description";
 import Options from "./components/Options/Options";
 import Feedback from "./components/Feedback/Feedback";
-import Notification from "./components/Notification";
+
+const initialState = {
+  good: 0,
+  neutral: 0,
+  bad: 0,
+};
 
 const getInitialClicks = () => {
   const savedClicks = window.localStorage.getItem("clicks");
 
-  return savedClicks !== null
-    ? JSON.parse(savedClicks)
-    : { good: 0, neutral: 0, bad: 0 };
+  return savedClicks !== null ? JSON.parse(savedClicks) : initialState;
 };
 
 export default function App() {
@@ -28,11 +31,7 @@ export default function App() {
   };
 
   const resetFeedback = () => {
-    setClicks({
-      good: 0,
-      neutral: 0,
-      bad: 0,
-    });
+    setClicks(initialState);
   };
 
   const totalFeedback = clicks.good + clicks.bad + clicks.neutral;
@@ -43,14 +42,14 @@ export default function App() {
 
   return (
     <>
-      <Descriptions />
+      <Description />
       <Options
         updateFeedback={updateFeedback}
         resetFeedback={resetFeedback}
         totalFeedback={totalFeedback}
       />
       {totalFeedback === 0 ? (
-        <Notification />
+        <p>No feedback yet</p>
       ) : (
         <Feedback
           clicks={clicks}
